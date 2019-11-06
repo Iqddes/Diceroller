cc767dc20f0c875e49ca1dea3e12f30c18405962
Iqddes <852702462iI>
2019-11-06 20:05:19 +0000

    All Parts are done

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -11,8 +11,12 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
@@ -55,46 +59,84 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-    public void on_button_click(View view){
+
+    //My Code
+
+   /* public void on_button_click(View view) {
+
+        int random2 = returnRandomNumber();
+        TextView viewRandom = this.findViewById(R.id.random);
+        viewRandom.setText(Integer.toString(random2));
+        roll_the_dice();
+
+
+    }*/
+
+    public void on_button_click3(View view){
 
         TextView tv = this.findViewById(R.id.NumberTextView);
-
         Random r = new Random();
+        int number=r.nextInt(6);
+        int random2 = returnRandomNumber();
 
-        int number = r.nextInt(7);
+
+
+        tv.setText(Integer.toString(random2));
         tv.setText(Integer.toString(number));
-        level2_3();
+
+
+
+         roll_the_dice();
+
     }
 
-    public void level2_3(){
+
+ public void on_button_click1(View view) {
+        TextView display = this.findViewById(R.id.random);
+
+       int question = returnRandomNumber() -1 ;
+       display.setText(Integer.toString(question));
+       TextView Game1 = this.findViewById(R.id.IceBreaker);
+
+        ArrayList<String> questionList = new ArrayList<String>();
+        questionList.add("If you could go anywhere in the world, where would you go?\n"); //Q0
+        questionList.add("If you were stranded on a desert island, what three things would you want to take with you?");//Q1
+        questionList.add("If you could eat only one food for the rest of your life, what would that be?\n");//Q2
+        questionList.add("If you won a million dollars, what is the first thing you would buy?\n");//Q3
+        questionList.add("If you could spaned the day with one fictional character, who would it be?\n");//Q4
+        questionList.add("If you found a magic lantern and a genie gave you three wishes, what would you wish?");//Q5
+
+        Game1.setText(questionList.get(question));
+
+    }
+
+
+   public static int returnRandomNumber() {
+
+        Random r = new Random();
+        int number = r.nextInt(6) + 1;
+        return number;
+
+    }
+
+
+    public void roll_the_dice() {
 
         TextView mssgoutput = this.findViewById(R.id.message);
         TextView tv = this.findViewById(R.id.NumberTextView);
         TextView inputt = this.findViewById(R.id.input);
         TextView score1 = this.findViewById(R.id.score);
 
-        int random = Integer.parseInt(tv.getText().toString());
+        int random3 = Integer.parseInt(tv.getText().toString());
         int inputUser = Integer.parseInt(inputt.getText().toString());
-        int score2  = Integer.parseInt(score1.getText().toString());
+        int score2 = Integer.parseInt(score1.getText().toString());
 
-        if (random == inputUser){
+        if (random3 == inputUser) {
             mssgoutput.setText("Congratulations!!");
-          score2++;
-          score1.setText(Integer.toString(score2));
-        }
-        else{
+            score2++;
+            score1.setText(Integer.toString(score2));
+        } else {
             mssgoutput.setText("Keep Trying");
         }
-
-
-
-
-
-
-
-
-
-
     }
 }
-
